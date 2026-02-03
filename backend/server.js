f2270b196c209c95d49afcc26db3eb0fe52b06dc
Redcatkolkata1 <redcatkolkata123@gmail.com>
2026-02-03 19:39:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal server error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 connectDB(process.env.MONGO_URI)
   .then(() => {
